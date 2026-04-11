b8731f3772608a9eec0b40881e873d92feab0bf4
Pedro Henrique Dias <pedrohenrique.dp05@gmail.com>
2026-04-11 13:39:07 -0300

    forçando build no netlify

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
-  // Manda o Next.js ignorar errinhos bobos de código e focar em colocar o site no ar
   eslint: {
     ignoreDuringBuilds: true,
   },
